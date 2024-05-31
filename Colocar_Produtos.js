59490fd1079f869628d2b5bbb76a85a54e00f9d5
pato14123 <henriqueinoue1000@gmail.com>
2024-05-31 18:33:32 -0300

    Projeto ta lento
    . adição da pasta 'daroca-api' com meu BD
    . mudança do lugar das imagens para a pasta 'imagens' do 'daroca-api'
    . criação do arquivo 'produtos.JSON', com informações dos produtos
    . não está funcionando as funções de colocar os produtos
    
    (precisa ter a extensão 'Live Server' ativada, única forma que consegui mexer com o '.JSON')

--- Colocar_Produtos.js
+++ Colocar_Produtos.js
@@ -1,56 +1,67 @@
 //Não funcionando
-$.getscript("produtos.js", function(){
-    todosprodutos();
-})
+
+function exibeDados(dados){
+    document.querySelector("#listaProdutos").innerHTML = dados[0].imagem
+}
+
 function colocar_produtos(tipo){
-            let produtos = todosprodutos(tipo)
-            console.log(produtos)
-            document.querySelector('#Selecionar_Produtos').innerHTML = ""
-            for(let indice = 0; indice < produtos.length; indice++){
+            // fetch('http://localhost:3000/produtos')
+            // .then(resposta => {
+            //     return resposta.json()
+            // })
+            // .then( (dados) => {
+            //     console.log(dados)
+            //     exibeDados(dados)
+            // })
 
-                //cria as divs
-                let div1 = document.createElement('div')
-                div1.setAttribute("class", "Produto",)
-                div1.setAttribute("id", produtos[indice].id)
-                document.querySelector('#Selecionar_Produtos').appendChild(div1)
+            fetch('http://127.0.0.1:5500/produtos.json')
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+        //     document.querySelector('#Selecionar_Produtos').innerHTML = ""
+        //     for(let indice = 0; indice < produtos.length; indice++){
 
-                //adcionar informações
-                //nome
-                let nome = document.createElement('p')
-                let node = document.createTextNode(produtos[indice].nome)
-                nome.appendChild(node)
-                document.getElementById(produtos[indice].id).appendChild(nome)
+        //         //cria as divs
+        //         let div1 = document.createElement('div')
+        //         div1.setAttribute("class", "Produto",)
+        //         div1.setAttribute("id", produtos[indice].id)
+        //         document.querySelector('#Selecionar_Produtos').appendChild(div1)
 
-                //preço
-                let preco = document.createElement('p')
-                node = document.createTextNode('R$' + produtos[indice].valor)
-                preco.appendChild(node)
-                document.getElementById(produtos[indice].id).appendChild(preco)
+        //         //adcionar informações
+        //         //nome
+        //         let nome = document.createElement('p')
+        //         let node = document.createTextNode(produtos[indice].nome)
+        //         nome.appendChild(node)
+        //         document.getElementById(produtos[indice].id).appendChild(nome)
 
-                //imagem
-                let imagem = document.createElement('img')
-                imagem.setAttribute("src", "Imagens/Produtos/" + produtos[indice].imagem)
-                document.getElementById(produtos[indice].id).appendChild(imagem)
+        //         //preço
+        //         let preco = document.createElement('p')
+        //         node = document.createTextNode('R$' + produtos[indice].valor)
+        //         preco.appendChild(node)
+        //         document.getElementById(produtos[indice].id).appendChild(preco)
 
-                //botões
-                let a = document.querySelector('.Selecionar_Produtos')
-                let div_Botoes = document.createElement('div')
-                div_Botoes.setAttribute('class', "Adicionar_Retirar")
-                document.getElementById(produtos[indice].id).appendChild(div_Botoes)
-                let botao_adicionar = document.createElement('button')
-                botao_adicionar.setAttribute('class', "Adicionar")
-                botao_adicionar.setAttribute('onclick', 'adicionar('+produtos[indice].id+')')
-                let botao_retirar = document.createElement('button')
-                botao_retirar.setAttribute('class', 'Retirar')
-                botao_retirar.setAttribute('onclick', 'retirar('+produtos[indice].id+')')
-                let quant = document.createElement('p')
-                quant.innerHTML = "0"
-                botao_adicionar.innerHTML = "+"
-                botao_retirar.innerHTML = "-"
-                document.getElementById(produtos[indice].id).querySelector('.Adicionar_Retirar').appendChild(botao_retirar)
-                document.getElementById(produtos[indice].id).querySelector('.Adicionar_Retirar').appendChild(quant)
-                document.getElementById(produtos[indice].id).querySelector('.Adicionar_Retirar').appendChild(botao_adicionar)
-                window.location.href = "#Selecionar_Produtos"
-            }
+        //         //imagem
+        //         let imagem = document.createElement('img')
+        //         imagem.setAttribute("src", "Imagens/Produtos/" + produtos[indice].imagem)
+        //         document.getElementById(produtos[indice].id).appendChild(imagem)
+
+        //         //botões
+        //         let a = document.querySelector('.Selecionar_Produtos')
+        //         let div_Botoes = document.createElement('div')
+        //         div_Botoes.setAttribute('class', "Adicionar_Retirar")
+        //         document.getElementById(produtos[indice].id).appendChild(div_Botoes)
+        //         let botao_adicionar = document.createElement('button')
+        //         botao_adicionar.setAttribute('class', "Adicionar")
+        //         botao_adicionar.setAttribute('onclick', 'adicionar('+produtos[indice].id+')')
+        //         let botao_retirar = document.createElement('button')
+        //         botao_retirar.setAttribute('class', 'Retirar')
+        //         botao_retirar.setAttribute('onclick', 'retirar('+produtos[indice].id+')')
+        //         let quant = document.createElement('p')
+        //         quant.innerHTML = "0"
+        //         botao_adicionar.innerHTML = "+"
+        //         botao_retirar.innerHTML = "-"
+        //         document.getElementById(produtos[indice].id).querySelector('.Adicionar_Retirar').appendChild(botao_retirar)
+        //         document.getElementById(produtos[indice].id).querySelector('.Adicionar_Retirar').appendChild(quant)
+        //         document.getElementById(produtos[indice].id).querySelector('.Adicionar_Retirar').appendChild(botao_adicionar)
+        //         window.location.href = "#Selecionar_Produtos"
+        //     }
         }
-alert("a")
