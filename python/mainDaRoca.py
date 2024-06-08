6652df407beac7076f233fa96522eff7a970b7ff
MatheusFranco2008 <162757230+MatheusFranco2008@users.noreply.github.com>
2024-06-08 19:36:33 -0300

    alterar produtos python pronto

--- python/mainDaRoca.py
+++ python/mainDaRoca.py
@@ -20,7 +20,7 @@ def ConectouAoBancoDeDados() -> bool: # informará se conseguiu (True) ou não (
 
 def inserir():
     meuCursor = conexao.cursor() # cursor: objeto de manipulação de dados
-    nome_produto = "1"
+    nome_produto = "-"
     while nome_produto != "0":
         nome_produto = input("Nome do Produto (0 para terminar): ")
         if nome_produto != "0":       # usuário não quer terminar o cadastro
@@ -46,9 +46,92 @@ def inserir():
  
         meuCursor.commit() # enviar as mudanças para o BD
 
+def alterar():
+    meuCursor = conexao.cursor() # objeto de manipulação de dados
+    nome_produto = "-"
+    while nome_produto != "0":
+        nome_produto = input("Nome do Produto (0 para terminar): ")
+        if nome_produto != "0": # usuário não quer terminar o cadastro
+            result = meuCursor.execute('SELECT nome, imagem, valor, descricao, categoria '+\
+                                      ' FROM daroca.produtos '+\
+                                      ' WHERE nome = ?', nome_produto)
+            registros = result.fetchall()      # fetchall recupera os dados e os armazena na variavel registros
+            if len(registros) == 0:
+                print("Produto não encontrado.")
+            else:
+                print("Produto encontrado:\n")
+                nome = registros[0][0]
+                imagem = registros[0][1]
+                valor = registros[0][2]
+                descricao = registros[0][3]
+                categoria = registros[0][4]
+
+                if nome != None:
+                    print("Nome do departamento: " + nome)
+                else:
+                    print("Nome não informado")
+
+                if imagem != None:
+                    print("imagem:" + imagem)
+                else:
+                    print("Imagem Não informada")
+
+                if valor != None:
+                    print("valor:" + valor)
+                else:
+                    print("Valor não informado")
+
+                if descricao != None:
+                    print("descição:" + descricao)
+                else:
+                    print("descrição Não informado")
+
+                if categoria != None:
+                    print("Categoria:" + categoria)
+                else:
+                    print("Categoria Não informada\n")
+
+                print("\nabaixo digite [enter] para manter a informação atual\n")
+                nome = input("Novo nome do Produto: ")
+                valor = input("Novo Valor do Produto:")
+                imagem = input("Nova imagem do Produto: ")
+                descricao = input("Nova descrição do Produto: ")
+                categoria = input("Nova Categoria do Produto: ")
+ 
+                if nome == "": # usuário digitou [Enter]
+                    nome = registros[0][0] # nome original do BD
+
+                if imagem == "": # usuário digitou [Enter]
+                    imagem = registros[0][1] # gerente original do BD
+
+                if valor == "": # usuário digitou [Enter]
+                    valor = registros[0][2] # data original do BD
+
+                if descricao == "": # usuário digitou [Enter]
+                    descricao = registros[0][3] # data original do BD
+
+                if categoria == "": # usuário digitou [Enter]
+                    categoria = registros[0][4] # data original do BD
+ 
+                # criamos uma string com o comando Update para atualizar os novos dados
+
+                sComando = "Update daroca.produtos " +\
+                " set nome = ?, imagem = ?, valor = ?, descricao = ?,"+\
+                " categoria = ? "+\
+                " where nome = ? "
+ 
+                # fazemos o cursor executar a string com o comando Update que criamos
+
+                try: # tente executar o comando abaixo:
+                    meuCursor.execute(sComando, nome, imagem, valor, descricao, categoria)
+                except: # em caso de erro
+                    print("Não foi possível incluir. Pode haver depto repetido.")
+
+                meuCursor.commit() # enviar as mudanças para o BD 
+
 def excluir():
     meuCursor = conexao.cursor() # objeto de manipulação de dados
-    nome_produto = "1"
+    nome_produto = "-"
     while nome_produto != "0":
         nome_produto = input("Nome do Produto (0 para terminar): ")
         if nome_produto != "0": 
